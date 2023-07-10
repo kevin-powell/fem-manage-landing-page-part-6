f7e6cfe4416ec36eac265a1b0cc495e1c9f722a7
VictorKachin <victor.kachin@yandex.ru>
2023-07-10 08:39:23 +0300

    create slider - finish

--- main.js
+++ main.js
@@ -9,3 +9,15 @@ navToggle.addEventListener('click', () => {
 	primaryNav.toggleAttribute('data-visible')
 	primaryHeader.toggleAttribute('data-overlay')
 })
+
+const slider = new A11YSlider(document.querySelector('.slider'), {
+	adaptiveHeight: false,
+	dots: true,
+	centerMode: true,
+	arrows: false,
+	responsive: {
+		480: {
+			dots: false, // dots enabled 1280px and up
+		},
+	},
+})
